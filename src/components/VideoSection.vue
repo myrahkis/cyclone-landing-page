2dfa1396205f814660bef1ddd482181ea2ea7f04
myrahkis <kruzinka@yandex.ru>
2025-04-08 17:39:48 +0300

    added video

--- src/components/VideoSection.vue
+++ src/components/VideoSection.vue
@@ -4,50 +4,14 @@ import SkrewImg from '@/ui/SkrewImg.vue'
 
 <template>
   <section class="video-section" id="overviews">
-    <SkrewImg />
-    <h2>Заголовок раздела</h2>
-    <div class="video-cards">
-      <div class="video-card">
-        <div class="video-elem">
-          <img src="/FloorVarient/floorVar1.png" alt="" class="img" />
-          <img src="/youtubePlaceholder.png" alt="" class="yt-placeholder" />
-        </div>
-        <div>
-          <h3>Заголовок раздела</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam varius tortor a tincidunt
-            convallis.
-          </p>
-        </div>
-      </div>
-      <div class="video-card">
-        <div class="video-elem">
-          <img src="/FloorVarient/floorVar2.png" alt="" class="img" /><img
-            src="/youtubePlaceholder.png"
-            alt=""
-            class="yt-placeholder"
-          />
-        </div>
-        <h3>Заголовок раздела</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam varius tortor a tincidunt
-          convallis.
-        </p>
-      </div>
-      <div class="video-card">
-        <div class="video-elem">
-          <img src="/WallVarient/wallVar3.png" alt="" class="img" /><img
-            src="/youtubePlaceholder.png"
-            alt=""
-            class="yt-placeholder"
-          />
-        </div>
-        <h3>Заголовок раздела</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam varius tortor a tincidunt
-          convallis.
-        </p>
-      </div>
+    <div class="video-elem">
+      <iframe
+        src="https://www.youtube.com/embed/ClvIqt0Vq5Y"
+        style="position: absolute; top: 0; left: 0; width: 100%; height: 100%"
+        frameborder="0"
+        allowfullscreen
+      >
+      </iframe>
     </div>
   </section>
 </template>
@@ -59,56 +23,22 @@ import SkrewImg from '@/ui/SkrewImg.vue'
   flex-direction: column;
   color: white;
   padding: 3.5rem;
-  /* min-height: 100vh; */
+  /* height: 85rem; */
   background: linear-gradient(to bottom, rgba(0, 0, 0), rgba(0, 0, 0, 0.4));
 }
-.video-cards {
-  display: flex;
-  flex-grow: 1;
-  gap: 2rem;
-}
 .video-elem {
+  overflow: hidden;
   position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
   border: 3px solid white;
   border-radius: 1.9rem;
-  height: 50%;
+  padding-bottom: 56.25%;
+  height: 0;
+  width: 100%;
   margin-bottom: 3rem;
 
   font-size: 2.5rem;
-}
-
-.img {
-  height: 100%;
-  max-width: 100%;
-}
-.yt-placeholder {
-  cursor: pointer;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 18%;
-}
-
-/* (480px–767px) */
-@media (max-width: 767px) {
-  .video-cards {
-    flex-direction: column;
-  }
-  .img {
-    max-width: 40%;
-  }
-  .yt-placeholder {
-    width: 11%;
-  }
-}
-/* (до 479px) */
-@media (max-width: 479px) {
-  .img {
-    max-width: 60%;
-  }
 }
 </style>
